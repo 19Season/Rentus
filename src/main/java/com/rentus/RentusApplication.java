d24c9b063001f301cadd78d5730ae832728b3589
prakashkunwar <prakash.kunwar@koerber.digital>
2021-03-07 13:09:36 +0100

    first commit

---
+++ src/main/java/com/rentus/RentusApplication.java
@@ -0,0 +1,13 @@
+package com.rentus;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentusApplication {
+
+	public static void main(String[] args) { SpringApplication.run(RentusApplication.class, args);
+	}
+
+}
